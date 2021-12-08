f5f6eed4fdac57451d527b2a9cd883ab771fa022
sadiaarfa <95604757+sadiaarfa@users.noreply.github.com>
2021-12-08 14:19:17 +0500

    Add files via upload
    Calculator

---
+++ python.py
@@ -0,0 +1,34 @@
+m= int(input("Enter the first number"))
+operator = input("Enter operator: ")
+n= int(input("Enter the second number"))
+if operator == "+":
+    print(m+n)   
+elif operator == "-":
+    print(m-n)
+elif operator == "*" or operator == "x":
+    print(m*n)
+elif operator == "/":
+    print(m/n)
+
+Total=int(input("enter total num"))
+Ob=int(input("enter the obtain marks"))
+percentage =Ob*100/Total
+print(percentage)
+
+
+Total=int(input("enter the total marks"))
+percentage=int(input("enter the percentage of marks"))
+marks = percentage/100*Total
+print(marks)
+
+
+
+  
+  
+    
+
+
+
+
+
+    
